f2c032b3b12f1e8ba44be7310b0523cf7c0dc0ad
Metamfitamin <65155881+Metamfitamin@users.noreply.github.com>
2023-01-28 22:13:30 +0300

    Create mergetwostrings.go
    merge two strings(strings already sorted)

---
+++ Strings/mergetwostrings.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func merge(s1 string, s2 string) string {
+  var s string
+  len1, len2 := len(s1), len(s2)
+  ptr1, ptr2 := 0, 0
+
+   for ptr1 < len1 && ptr2 < len2 {
+    if s1[ptr1] < s2[ptr2] {
+      s += string(s1[ptr1])
+      ptr1++
+    } else {
+      s += string(s2[ptr2])
+      ptr2++
+    }
+  }
+
+  if ptr1 < len1 {
+    s += s1[ptr1:]
+  }
+
+  if ptr2 < len2 {
+    s += s2[ptr2:]
+  }
+
+  return s
+}
+
+func main() {
+    var s1 string
+    var s2 string
+    fmt.Scanln(&s1)
+    fmt.Scanln(&s2)
+    fmt.Println(merge(s1, s2))
+}
